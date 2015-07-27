7cefe64a607c67fc14c603239c4b612abebb76e3
J4ken <jojje133@gmail.com>
2015-07-27 22:32:31 +0200

    Added Player

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	    Board board = new Board(15,17);
+        Player player_1 = new Player(0,0,10,1,1);
 
+        System.out.println(player_1.x);
         System.out.println(board.height);
     }
 
--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -4,4 +4,15 @@ package com.company;
  * Created by Jacob on 2015-07-27.
  */
 public class Player {
+
+    static int x, y, speed, bombs, power;
+
+    public Player(int x, int y, int speed, int bombs, int power){
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.bombs = bombs;
+        this.power = power;
+    }
+
 }
