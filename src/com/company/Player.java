1a36c9ef27f1fdd26e55ab7b798330eb79ebebec
hakangud <hakangud@gmail.com>
2015-07-28 23:22:29 +0200

    tagit bort klassen Graphics, lagt till klassen Game och lagt till lite keyhandling i den

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -23,4 +23,51 @@ public class Player {
         }
     }
 
+    public static int getxPos() {
+        return xPos;
+    }
+
+    public static void setxPos(int xPos) {
+        Player.xPos = xPos;
+    }
+
+    public static int getyPos() {
+        return yPos;
+    }
+
+    public static void setyPos(int yPos) {
+        Player.yPos = yPos;
+    }
+
+    public static int getBombs() {
+        return bombs;
+    }
+
+    public static void setBombs(int bombs) {
+        Player.bombs = bombs;
+    }
+
+    public static int getHealth() {
+        return health;
+    }
+
+    public static void setHealth(int health) {
+        Player.health = health;
+    }
+
+    public static int getPower() {
+        return power;
+    }
+
+    public static void setPower(int power) {
+        Player.power = power;
+    }
+
+    public static int getSpeed() {
+        return speed;
+    }
+
+    public static void setSpeed(int speed) {
+        Player.speed = speed;
+    }
 }
