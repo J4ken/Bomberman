836a6b2bc596c966a7c7bccb8a9e5563c7f077b7
hakangud <hakangud@gmail.com>
2015-07-30 22:48:21 +0200

    lagt till klassen Action

---
+++ src/com/company/Bomb.java
@@ -0,0 +1,27 @@
+package com.company;
+
+/**
+ * Created by Håkan on 2015-07-30.
+ */
+public class Bomb {
+    final static int timer = 3000;
+    private  int xPos, yPos, power;
+    Board board;
+
+    public Bomb(Player p, Board b) {
+        this.xPos = p.getxPos();
+        this.yPos = p.getyPos();
+        this.power = 1;
+        this.board = b;
+
+    }
+
+    public void blow(Bomb bomb) {
+        for (int i = 0; i < power; ++i) {
+            for (int j = 0; j < power; ++j) {
+                if (board.getTile())
+            }
+        }
+    }
+
+}
