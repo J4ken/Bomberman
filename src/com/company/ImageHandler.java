90ed27adf75d97f44d5cff05aedf4626015341d8
J4ken <jojje133@gmail.com>
2015-08-14 13:35:01 +0200

    Merge branch 'master' of D:\Programmering\Java\Bomberman with conflicts.

---
+++ src/com/company/ImageHandler.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import javax.swing.*;
+
+/**
+ * Created by Jacob on 2015-08-10.
+ */
+public class ImageHandler {
+
+    public ImageIcon i;
+
+
+    public void loadImages(){
+        
+
+    }
+
+    public ImageIcon getImage(Tiles tile, int time){
+
+        switch (tile){
+
+            case PLAYER1:
+
+                break;
+
+            case PLAYER2:
+
+                break;
+        }
+
+
+        return i;
+    }
+
+}
